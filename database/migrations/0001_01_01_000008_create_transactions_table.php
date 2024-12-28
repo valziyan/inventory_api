3fad58932b2c82f3ab1117a6f4dcb35fdf61a012
valziyan <valziyan@gmail.com>
2024-12-28 22:33:43 +0800

    first commit, database migrations established

---
+++ database/migrations/0001_01_01_000008_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('inventory_id')->constrained('inventories');
+            $table->enum('type', ['stock_in', 'stock_out', 'adjustment']);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
